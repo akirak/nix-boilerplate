eb3e14f78b144e524b34dd6f2f7d0a7d2818c759
Akira Komamura <akira.komamura@gmail.com>
2020-08-29 22:47:42 +0900

    Add default.nix for copying source files

---
+++ default.nix
@@ -0,0 +1,21 @@
+{ pkgs ? import <nixpkgs> {} }:
+let
+  # gitignoreSource = import ./nix/gitignore.nix;
+  gitignoreFilter = (
+    import (import ./nix/sources.nix)."gitignore.nix" {
+      inherit (pkgs) lib;
+    }
+  ).gitignoreFilter;
+  f = path: type:
+    (gitignoreFilter ./. path type)
+    && ! builtins.elem (baseNameOf path)
+      [
+        "default.nix"
+        "nix-boilerplate.el"
+        "README.md"
+      ];
+in
+pkgs.srcOnly {
+  name = "nix-boilerplate";
+  src = builtins.filterSource f ./.;
+}
